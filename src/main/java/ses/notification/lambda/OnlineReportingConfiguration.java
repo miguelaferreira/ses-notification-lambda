a3aebabe5b80646ba58babd5a5fd66305d07d2de
Miguel Ferreira <miguelferreira@me.com>
2023-09-18 16:52:20 +0200

    fixup! Rename configuration parameters to split online from offline reporting

--- src/main/java/ses/notification/lambda/OnlineReportingConfiguration.java
+++ src/main/java/ses/notification/lambda/OnlineReportingConfiguration.java
@@ -1,7 +1,9 @@
 package ses.notification.lambda;
 
 import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.context.annotation.Requires;
 
+@Requires(property = OnlineReportingConfiguration.PREFIX)
 @ConfigurationProperties(OnlineReportingConfiguration.PREFIX)
 public record OnlineReportingConfiguration(String from, String to) {
 
